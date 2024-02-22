782c17d311e0a0750b41630e8f290dd76642c79f
jder053@uottawa.ca <jder053@uottawa.ca>
2024-02-22 18:36:39 -0500

    created footer and header elements

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <h1 className="text-4xl">Eventifyr</h1>
       <Button>Hello</Button>
     </main>
   );
