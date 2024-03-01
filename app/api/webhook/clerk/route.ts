6bb96a87d332bdc7186fe2d38e011d4c624a4267
jder053@uottawa.ca <jder053@uottawa.ca>
2024-03-01 12:07:44 -0500

    worked on webhooks

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,6 +1,9 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { createUser } from '@/lib/actions/user.actions'
+import { clerkClient } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
  
 export async function POST(req: Request) {
  
@@ -63,7 +66,7 @@ export async function POST(req: Request) {
       photo: image_url,
     }
 
-    //const newUser = await createUser(user);
+    const newUser = await createUser(user);
 
     if(newUser) {
         await clerkClient.users.updateUserMetadata(id, {
