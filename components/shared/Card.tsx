7db42c0608fd4a82e44483a6afb7ee8711c5addc
jder053@uottawa.ca <jder053@uottawa.ca>
2024-03-06 17:33:50 -0500

    created card design template

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -15,56 +15,33 @@ const Card = () => {
 
   return (
     <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
-      <Link
-        href={"www.google.com"}
-        // style={{ backgroundImage: `url(${event.imageUrl})` }}
-        className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
+      <Image
+        src="/assets/images/parliament.jpg"
+        alt="parliament building"
+        width={1000}
+        height={1000}
+        className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
       />
-
-      <div className="absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all">
-        {/* <Link href={`/events/${event._id}/update`}>
-          <Image
-            src="/assets/icons/edit.svg"
-            alt="edit"
-            width={20}
-            height={20}
-          />
-        </Link> */}
-      </div>
 
       <div className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4">
         <div className="flex gap-2">
-          <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
-            100
-          </span>
-          <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
-            EVENT AND CATEGORY NAME
+          <p className="p-semibold-14 w-min rounded-full bg-purple-500/50 px-4 py-1 text-grey-500 line-clamp-1">
+            FREE
           </p>
         </div>
 
-        <p className="p-medium-16 p-medium-18 text-grey-500">
-          {/* {formatDateTime(event.startDateTime).dateTime} */}
-        </p>
-
-        {/* <Link href={`/events/${event._id}`}>
+        <Link href={`/events/`}>
           <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
-            {event.title}
+            Visit the Parliament building
           </p>
-        </Link> */}
+        </Link>
 
         <div className="flex-between w-full">
           <p className="p-medium-14 md:p-medium-16 text-grey-600">
             Organized by Bob Smith
           </p>
-
-          <p className="text-primary-500">Order Details</p>
-          <Image
-            src="/assets/icons/arrow.svg"
-            alt="search"
-            width={10}
-            height={10}
-          />
         </div>
+        <p className="p-medium-16 p-medium-18 text-grey-500">Learn More</p>
       </div>
     </div>
   );
